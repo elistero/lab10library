53bfd13323205aebe53f0f5f67ee24c8f9665f57
elsa krauskeite <76253481+elistero@users.noreply.github.com>
2021-04-12 11:54:27 +0300

    Update QuEquationTest.php

--- tests/QuEquationTest.php
+++ tests/QuEquationTest.php
@@ -10,16 +10,17 @@ class QuEquationTest extends TestCase
     {
         $quequation = new QuEquation();
         $this->expectExceptionMessage("Ошибка: уравнение не имеет корней.");
+        $this->expectExceptionMessage("Ошибка: уравнения не существует.");
+        $quequation->solve(7, 2, 4);
+        $quequation->solve(5, 2, 3);
         $this->expectException(Exception::class);
-        $quequation->solve(4, 0, 8);
-        $quequation->solve(4, 2, 1);
     }
 
     public function testQuEquation()
     {
         $quequation = new QuEquation();
-        $this->assertEquals([-5, 5], $quequation->solve(-3, 0, 75));
+        $this->assertEquals([10, 60], $quequation->solve(1, -70, 600));
         $this->assertEquals([-3], $quequation->solve(0, 3, 9));
-        $this->assertEquals([-3], $quequation->solve(1, 6, 9));
+        $this->assertEquals([3], $quequation->solve(3, -18, 27));
     }
 }
